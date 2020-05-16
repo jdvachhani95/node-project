3a1aed40c6fa8792a1fceabf129ef36ad14d8cd3
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-05-16 15:32:17 -0400

    add mongodb credentials

--- src/environment.ts
+++ src/environment.ts
@@ -5,6 +5,10 @@ interface Environment {
     introspection: boolean;
     playground: boolean;
   };
+  mongoDb: {
+    databaseName: string;
+    url: string;
+  };
   port: number | string;
 }
 
@@ -12,6 +16,10 @@ export const environment: Environment = {
   apollo: {
     introspection: process.env.APOLLO_INTROSPECTION === 'true',
     playground: process.env.APOLLO_PLAYGROUND === 'true',
+  },
+  mongoDb: {
+    databaseName: process.env.MONGODB_DB_NAME as string,
+    url: process.env.MONGODB_URL as string,
   },
   port: process.env.PORT || defaultPort,
 };
