c75b122e2dcf1cbdb565eab5ec7b384bd6661ebb
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-05-16 15:33:48 -0400

    Connect mongodb with server

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { ApolloServer } from 'apollo-server';
-
 import { environment } from './environment';
-import resolvers from './resolvers';
-import typeDefs from './type-defs';
+import resolvers from './API/resolvers/dataConversion.resolver';
+import typeDefs from './API/schemas/dataConversion.graphql';
+import { mongoDbProvider } from './mongodb.provider';
 
-const server = new ApolloServer({
-  resolvers,
-  typeDefs,
-  introspection: environment.apollo.introspection,
-  playground: environment.apollo.playground,
-});
+(async function bootstrapAsync(): Promise<void> {
+  await mongoDbProvider.connectAsync(environment.mongoDb.databaseName);
+  // await addMockUsersAsync(); // TODO: Remove in PROD.
 
-server
-  .listen(environment.port)
-  .then(({ url }) => console.log(`Server ready at ${url}. `));
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    introspection: environment.apollo.introspection,
+    playground: environment.apollo.playground,
+  });
 
-if (module.hot) {
-  module.hot.accept();
-  module.hot.dispose(() => console.log('Module disposed. '));
-}
+  server
+    .listen(environment.port)
+    .then(({ url }) => console.log(`Server ready at ${url}. `));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(async () => {
+      server.stop();
+      await mongoDbProvider.closeAsync();
+    });
+  }
+})();
