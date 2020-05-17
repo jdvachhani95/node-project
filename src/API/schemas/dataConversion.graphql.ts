18e7568358a59a75f1dff228ae816fc333a8cf1d
jdvachhani95 <jaydeepvachhani22@gmail.com>
2020-05-16 23:46:07 -0400

    Update GetVehicleMakes query and types

--- src/API/schemas/dataConversion.graphql.ts
+++ src/API/schemas/dataConversion.graphql.ts
@@ -1,10 +1,35 @@
 import { gql } from 'apollo-server';
 
 export default gql`
+  type AllVehicleMakesInfo {
+    """
+    It's a Unix timestamp
+    """
+    LastUpdated: Int
+    """
+    AllVehicleMake is a array of all vehicle makes
+    """
+    AllVehicleMakes: [VehicleMakeInfo]
+  }
+  type VehicleMakeInfo {
+    Make_ID: String
+    Make_Name: String
+    """
+    VehicleTypesForMakeIds is a array of all types of vehicle for the particular MakeId
+    """
+    VehicleTypesForMakeIds: [VehicleTypesInfo]
+  }
+  type VehicleTypesInfo {
+    VehicleTypeId: String
+    VehicleTypeName: String
+  }
   type Query {
     """
-    Test Message.
+    Query return Array of all vehicle make objects provided from this
+    link: https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=XML and
+    update full document on mondoDB database
+    *Note: Mutation take approx 30 min to fetch all vehicle types for makeIds and combine complete doc
     """
-    xmlToJson: String!
+    GetVehicleMakes: AllVehicleMakesInfo!
   }
 `;
